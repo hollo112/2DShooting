051c73f7ab33b4c68b9647bba7116ce7cbbc6861
hollo112 <76999794+hollo112@users.noreply.github.com>
2025-11-04 11:06:13 +0900

    총알 가속도 적용

---
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("이동속도")]
+    public float FirstSpeed = 1f;
+    public float LastSpeed = 7f;
+    private float _speed;
+
+    [Header("가속시간")]
+    public float AccerationTime = 1.2f;
+    private float _elapsedTime = 0f;
+
+    void Start()
+    {
+        _speed = FirstSpeed;
+    }
+
+    void Update()
+    {
+        AccelerateBullet();
+        MoveBullet();
+    }
+
+    void AccelerateBullet()
+    {
+        if (_elapsedTime < AccerationTime)
+        {
+            _elapsedTime += Time.deltaTime;
+            float t = _elapsedTime / AccerationTime; // 0 ~ 1
+            _speed = Mathf.Lerp(FirstSpeed, LastSpeed, t);
+        }
+    }
+
+    void MoveBullet()
+    {
+        Vector2 position = transform.position;
+        Vector2 newPosition = position + Vector2.up * _speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -4,14 +4,6 @@ using static UnityEngine.UI.Image;
 // Player 이동
 public class PlayerMove : MonoBehaviour
 {
-    // 목표
-    // "키보드 입력"에 따라 "방향"을 구하고 그 방향으로 이동
-
-    // 구현 순서 :
-    // 1. 키보드 입력을 받는다.
-    // 2. 방향을 구한다.
-    // 3. 이동한다.
-
     // 필요 속성:
     [Header("능력치")]
     public float Speed = 3f;    // 속력
@@ -20,19 +12,21 @@ public class PlayerMove : MonoBehaviour
     public float MinSpeed = 1f;
     public float SpeedMultiplier = 1.2f;    // Shift키 누르면 속도 배 상승
 
+    [Header("원점 좌표")]
+    private Vector2 _originPosition = new Vector2(0f, -2.5f); // 원점 좌표
+
     [Header("이동 범위")]
     public float MinX = -2.4f;
     public float MaxX = 2.4f;
     public float MinY = -5f;
     public float MaxY = 0f;
-    public Vector2 Origin = new Vector2(0f, -2.5f); // 원점 좌표
 
     private bool isReturningToOrigin = false; // 원점으로 이동 중인지 여부
 
     // 게임 오브젝트가 게임을 시작할 때
     private void Start()
     {
-        
+        _originPosition = transform.position; // 현재 위치를 원점으로 설정
     }
 
     // 게임 오브젝트가 게임을 시작 후 프레임마다
@@ -46,7 +40,7 @@ public class PlayerMove : MonoBehaviour
     {
         ChangeSpeed();
         SpeedUpShift();
-        MoveToOrigin();
+        TurnOnMovingOrigin();
     }
 
     private void ChangeSpeed()
@@ -73,7 +67,7 @@ public class PlayerMove : MonoBehaviour
             Speed /= SpeedMultiplier;
         }
     }
-    private void MoveToOrigin()
+    private void TurnOnMovingOrigin()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
@@ -110,7 +104,7 @@ public class PlayerMove : MonoBehaviour
     {
         if (isReturningToOrigin)
         {
-            Vector2 directionToOrigin = Origin - currentPosition;
+            Vector2 directionToOrigin = _originPosition - currentPosition;
             return directionToOrigin.normalized;
         }
         else
