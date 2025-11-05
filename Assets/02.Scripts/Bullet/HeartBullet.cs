3580845249cefc56d0575fbec5313372678481dc
hollo112 <76999794+hollo112@users.noreply.github.com>
2025-11-05 14:52:09 +0900

    EnemySpawner 구현

---
+++ Assets/02.Scripts/Bullet/HeartBullet.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class HeartBullet : Bullet
+{
+    private float _elapsedTime = 0f; // 누적 시간
+    private Vector2 _startPosition;
+    protected override void Start()
+    {
+        base.Start();
+        _startPosition = transform.position;
+    }
+
+    public override void MoveBullet()
+    {
+        // 누적 시간 증가
+        _elapsedTime += Time.deltaTime;
+
+        // 하트 계산
+        float x = _elapsedTime;
+        float inputY = Mathf.Clamp(_elapsedTime, -1f, 1f);
+        float y = inputY + Mathf.Sqrt(1 - Mathf.Pow(inputY, 2));
+        Vector2 perpendicular = Vector2.zero;
+        switch (Direction)
+        {
+            case BulletDirection.Up:
+            case BulletDirection.Down:
+                perpendicular = new Vector2(x, y); 
+                break;
+            case BulletDirection.Left:
+            case BulletDirection.Right:
+                perpendicular = new Vector2(y, x); 
+                break;
+        }
+
+        Vector2 newPosition = _startPosition + perpendicular;
+        transform.position = newPosition;
+    }
+}
--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -1,16 +1,40 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Enemy 프리팹")]
+    public GameObject EnemyPrefab;
+
+    [Header("생성 주기")]
+    [SerializeField] private float _minRandomValue = 1f;
+    [SerializeField] private float _maxRandomValue = 3f;
+    private float SpawnCooltime;
+    private float _spawnTimer = 0f;
+
+    private void Start()
     {
-        
+        SetCooltimeRandom();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _spawnTimer += Time.deltaTime;
+        if (_spawnTimer < SpawnCooltime)
+        {
+            return;
+        }
+
+        GameObject enemyObject = Instantiate(EnemyPrefab);
+        enemyObject.transform.position = transform.position;
+       
+        SetCooltimeRandom();
+
+        _spawnTimer = 0f;
+    }
+
+    void SetCooltimeRandom()
+    {
+        float randomCooltime = UnityEngine.Random.Range(_minRandomValue, _maxRandomValue);
+        SpawnCooltime = randomCooltime;
     }
 }
