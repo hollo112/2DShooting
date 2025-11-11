77627ebd39e947988c0a0478951eb70208eb626e
hollo112 <76999794+hollo112@users.noreply.github.com>
2025-11-11 13:16:04 +0900

    feat: 배경 머터리얼 설정

---
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    // 배경 스크롤
+    // 필요 속성
+    // -머터리얼
+    // -스크롤 속도
+
+    private Material _material;
+
+    void Start()
+    {
+        _material = GetComponent<Material>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
