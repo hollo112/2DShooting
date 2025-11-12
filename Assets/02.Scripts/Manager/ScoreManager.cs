afc327aae20d1a82de9659af050ca3f9078751dc
hollo112 <76999794+hollo112@users.noreply.github.com>
2025-11-12 11:06:15 +0900

    feat: 점수시스템 UI
    - 적을 죽이면 점수가 올라간다

--- Assets/02.Scripts/Manager/ScoreManager.cs
+++ Assets/02.Scripts/Manager/ScoreManager.cs
@@ -1,16 +1,31 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // 응집도를 높여라
+    // 응집도 : '데이터'와 '데이터를 조작하는 로직'이 얼마나 잘 모여있나
+    // 응집도를 높이고, 필요한 것만 외부에 공개하는 것을 '캡슐화'
+    [SerializeField] private Text _currentScoreTextUI;
+    private int _currentScore = 0;
+
+    private void Start()
     {
-        
+        Refresh();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddScore(int score)
     {
-        
+        if (score <= 0) return;
+
+        _currentScore += score;
+
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+         
+        _currentScoreTextUI.text = $"현재 점수: {_currentScore:N0}";
     }
 }
